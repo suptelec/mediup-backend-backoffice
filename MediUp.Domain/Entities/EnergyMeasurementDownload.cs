3f5e595f890b48349359545de1dd7d42c1f42696
suptelec <suptelec@gmail.com>
2025-12-27 10:31:41 -0500

    Add energy measurement entities

---
+++ MediUp.Domain/Entities/EnergyMeasurementDownload.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MediUp.Domain.Entities;
+
+public class EnergyMeasurementDownload : BaseEntity
+{
+    public string Username { get; set; } = null!;
+
+    public string Meter { get; set; } = null!;
+
+    public DateOnly MeasurementDate { get; set; }
+
+    public string IntegrationStatus { get; set; } = null!;
+
+    public virtual ICollection<EnergyMeasurementEvent> Events { get; set; } = new List<EnergyMeasurementEvent>();
+
+    public virtual ICollection<EnergyMeasurementData> Data { get; set; } = new List<EnergyMeasurementData>();
+}
