1c31b12e708d3396194151fa0656a3805a6a5a1b
Santiago Vizcaino <jorge_santiagovizcaino@hotmail.com>
2026-01-15 15:25:58 -0500

    add transformadores

--- MediUp.Infrastructure/Persistence/AppDbContext.cs
+++ MediUp.Infrastructure/Persistence/AppDbContext.cs
@@ -24,7 +24,7 @@ public class AppDbContext(DbContextOptions<AppDbContext> options, ILogger<AppDbC
     public DbSet<EnergyMeasurementEvent> EnergyMeasurementEvents => Set<EnergyMeasurementEvent>();
     public DbSet<EnergyMeasurementData> EnergyMeasurementData => Set<EnergyMeasurementData>();
     public DbSet<LigtherTransformer> LigtherTransformers => Set<LigtherTransformer>();
-    public DbSet<LigtherMetric> LigtherMetrics => Set<LigtherMetric>();
+    public DbSet<LigtherMetric> LigtherMetric => Set<LigtherMetric>();
     public DbSet<LigtherTransformerMetric> LigtherTransformerMetrics => Set<LigtherTransformerMetric>();
 
 
