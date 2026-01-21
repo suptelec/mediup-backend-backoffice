6fb92f46001b34b4ba5e38ae414eeb7af4908732
Santiago Vizcaino <jorge_santiagovizcaino@hotmail.com>
2026-01-21 10:34:32 -0500

    add migration

---
+++ MediUp.Infrastructure/Migrations/20260121153332_UpdateLgthers.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediUp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateLgthers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UrlCertificate",
+                schema: "mup",
+                table: "LigtherTransformers",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UrlCertificate",
+                schema: "mup",
+                table: "LigtherMetric",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AuthorizationDocument",
+                schema: "mup",
+                table: "ElectriCompany",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UrlCertificate",
+                schema: "mup",
+                table: "LigtherTransformers");
+
+            migrationBuilder.DropColumn(
+                name: "UrlCertificate",
+                schema: "mup",
+                table: "LigtherMetric");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorizationDocument",
+                schema: "mup",
+                table: "ElectriCompany");
+        }
+    }
+}
