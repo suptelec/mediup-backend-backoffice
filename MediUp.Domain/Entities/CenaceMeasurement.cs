0012228ab01a4922302a5a77fb88bd1e05641c01
Santiago Vizcaino <jorge_santiagovizcaino@hotmail.com>
2026-03-05 15:59:26 -0500

    persistenciua de las mediciones de cenace

---
+++ MediUp.Domain/Entities/CenaceMeasurement.cs
@@ -0,0 +1,12 @@
+﻿namespace MediUp.Domain.Entities;
+public class CenaceMeasurement: BaseEntity
+{
+    public DateOnly MeasurementDate { get; set; }
+    public DateTime PublicationDate { get; set; }
+    public string CenaceCode { get; set; } = null!;
+    public string TPLCode { get; set; } = null!;
+    public string Code { get; set; } = null!;
+    public virtual ICollection<CenaceMeasurementDetail> CenaceMeasurementDetails { get; set; } = new List<CenaceMeasurementDetail>();
+
+
+}
---
+++ MediUp.Domain/Entities/CenaceMeasurementDetail.cs
@@ -0,0 +1,8 @@
+﻿namespace MediUp.Domain.Entities;
+public class CenaceMeasurementDetail: BaseEntity
+{
+    public string Hour { get; set; } = string.Empty;
+    public double Value { get; set; }
+    public long CenaceMeasurementId { get; set; }
+    public virtual CenaceMeasurement CenaceMeasurement { get; set; } = null!;
+}
