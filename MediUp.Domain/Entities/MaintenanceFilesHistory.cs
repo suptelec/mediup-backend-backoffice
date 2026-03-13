d081124dd375d985f4341da3abb20a9c034c3dbb
Santiago Vizcaino <jorge_santiagovizcaino@hotmail.com>
2026-03-13 14:29:29 -0500

    cronograma match con preventivo

--- MediUp.Domain/Entities/MaintenanceFilesHistory.cs
+++ MediUp.Domain/Entities/MaintenanceFilesHistory.cs
@@ -1,11 +1,12 @@
 ﻿namespace MediUp.Domain.Entities;
 public class MaintenanceFilesHistory: BaseEntity
 {
-
+    public int MonthNumber { get; set; }
+    public int WorkOrderhNumber { get; set; }
+    public int Year { get; set; }
+    public string SystemLigtherName { get; set; } = null!;
     public string HtmlContent { get; set; } = null!;
-
     public string MaintenanceType { get; set; } = null!;
-
     public string? ElectricCompanyName { get; set; }
 
 
---
+++ MediUp.Infrastructure/Migrations/20260313184402_UpdateEntity-MaintenanceFilesHistory-LinkCronograma.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediUp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEntityMaintenanceFilesHistoryLinkCronograma : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MonthNumber",
+                schema: "mup",
+                table: "MaintenanceFilesHistories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SystemLigtherName",
+                schema: "mup",
+                table: "MaintenanceFilesHistories",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "WorkOrderhNumber",
+                schema: "mup",
+                table: "MaintenanceFilesHistories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Year",
+                schema: "mup",
+                table: "MaintenanceFilesHistories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MonthNumber",
+                schema: "mup",
+                table: "MaintenanceFilesHistories");
+
+            migrationBuilder.DropColumn(
+                name: "SystemLigtherName",
+                schema: "mup",
+                table: "MaintenanceFilesHistories");
+
+            migrationBuilder.DropColumn(
+                name: "WorkOrderhNumber",
+                schema: "mup",
+                table: "MaintenanceFilesHistories");
+
+            migrationBuilder.DropColumn(
+                name: "Year",
+                schema: "mup",
+                table: "MaintenanceFilesHistories");
+        }
+    }
+}
