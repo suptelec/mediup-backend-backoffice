bd6df776b5fef4fb4f6bd9e3e82547a9bb7425b5
Santiago Vizcaino <jorge_santiagovizcaino@hotmail.com>
2026-03-19 18:42:47 -0500

    nulleable para HtmlContent

--- MediUp.Domain/Entities/MaintenanceFilesHistory.cs
+++ MediUp.Domain/Entities/MaintenanceFilesHistory.cs
@@ -5,7 +5,7 @@ public class MaintenanceFilesHistory: BaseEntity
     public int WorkOrderhNumber { get; set; }
     public int Year { get; set; }
     public string SystemLigtherName { get; set; } = null!;
-    public string HtmlContent { get; set; } = null!;
+    public string? HtmlContent { get; set; } 
     public byte[]? PdfContent { get; set; }
     public string MaintenanceType { get; set; } = null!;
     public string? ElectricCompanyName { get; set; }
