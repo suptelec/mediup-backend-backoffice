c7321c61d7240e96f5343fd1b89d2c551537a7fc
Santiago Vizcaino <jorge_santiagovizcaino@hotmail.com>
2026-03-19 18:48:56 -0500

    html content null

---
+++ MediUp.Infrastructure/Migrations/20260319234343_UpdateEntity-MaintenanceFilesHistory-HtmlContent.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediUp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEntityMaintenanceFilesHistoryHtmlContent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "HtmlContent",
+                schema: "mup",
+                table: "MaintenanceFilesHistories",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "mup",
+                table: "MaintenanceFilesHistories",
+                keyColumn: "HtmlContent",
+                keyValue: null,
+                column: "HtmlContent",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HtmlContent",
+                schema: "mup",
+                table: "MaintenanceFilesHistories",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
