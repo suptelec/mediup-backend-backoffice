c23f4aac16ab48324f2f9d14bdf79ec578949a18
Santiago Vizcaino <jorge_santiagovizcaino@hotmail.com>
2026-03-22 11:29:26 -0500

    no blob

---
+++ MediUp.Infrastructure/Migrations/20260322155631_Noblobs.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MediUp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Noblobs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PdfContent",
+                schema: "mup",
+                table: "MaintenanceFilesHistories");
+
+            migrationBuilder.RenameColumn(
+                name: "HtmlContent",
+                schema: "mup",
+                table: "MaintenanceFilesHistories",
+                newName: "PdfContentUrl");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PdfContentUrl",
+                schema: "mup",
+                table: "MaintenanceFilesHistories",
+                newName: "HtmlContent");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PdfContent",
+                schema: "mup",
+                table: "MaintenanceFilesHistories",
+                type: "longblob",
+                nullable: true);
+        }
+    }
+}
