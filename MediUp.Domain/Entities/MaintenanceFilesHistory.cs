7c53d86b1d339129e1800e37f15f320e0e3d6156
Santiago Vizcaino <jorge_santiagovizcaino@hotmail.com>
2026-03-22 10:56:09 -0500

    ya no blob

--- MediUp.Domain/Entities/MaintenanceFilesHistory.cs
+++ MediUp.Domain/Entities/MaintenanceFilesHistory.cs
@@ -5,8 +5,7 @@ public class MaintenanceFilesHistory: BaseEntity
     public int WorkOrderhNumber { get; set; }
     public int Year { get; set; }
     public string SystemLigtherName { get; set; } = null!;
-    public string? HtmlContent { get; set; } 
-    public byte[]? PdfContent { get; set; }
+    public string? PdfContentUrl { get; set; }
     public string MaintenanceType { get; set; } = null!;
     public string? ElectricCompanyName { get; set; }
     public string? ReportData { get; set; }
