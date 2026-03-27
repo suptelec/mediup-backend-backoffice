049c5d267fbf3dca803cc894c22e9404dfb98882
Santiago Vizcaino <jorge_santiagovizcaino@hotmail.com>
2026-03-27 09:47:16 -0500

    decimales controlados

---
+++ MediUp.Infrastructure/Persistence/EntitiesConfiguration/EnergyMeasurementSummaryConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using MediUp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediUp.Infrastructure.Persistence.EntitiesConfiguration;
+public class EnergyMeasurementSummaryConfiguration : IEntityTypeConfiguration<EnergyMeasurementSummary>
+{
+    public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<EnergyMeasurementSummary> builder)
+    {
+        builder.Property(x => x.TotalActiveEnergyDeliveredKwh).HasPrecision(18, 4);
+        builder.Property(x => x.TotalActiveEnergyReceivedKwh).HasPrecision(18, 4);
+        builder.Property(x => x.TotalReactiveEnergyDeliveredKvarh).HasPrecision(18, 4);
+        builder.Property(x => x.TotalReactiveEnergyReceivedKvarh).HasPrecision(18, 4);
+        builder.Property(x => x.TotalApparentEnergyDeliveredKvah).HasPrecision(18, 4);
+    }
+}
